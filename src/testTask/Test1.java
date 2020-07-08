42c536e2f81937fd1cc218773e0813c60f412dfa
akuliniya <akuliniya@mail.ru>
2020-07-09 00:51:13 +0300

    bug fixed: The dot after "Hello" is replaced with a comma

--- src/testTask/Test1.java
+++ src/testTask/Test1.java
@@ -2,6 +2,6 @@ package testTask;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello. Fox!");
+        System.out.println("Hello, Fox!");
     }
 }
