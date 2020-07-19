948bc3d3dfe575589c5fd8f869a29ee7d71f5902
akuliniya <akuliniya@mail.ru>
2020-07-19 20:37:13 +0300

    Third  attempt to add Final Task 1

--- src/testTask/FinalTask1.java
+++ src/testTask/FinalTask1.java
@@ -1,4 +1,70 @@
 package testTask;
+/*
+Ввести с консоли число в бинарном формате.
+Перевести его в десятичный формат, записать в переменную int и вывести на экран.
+Необходимо использовать циклы, нельзя использования готовые методы языка Java,
+для перевода числа из одной системы счисления в другую.
+
+Прошу, прощенья, я просто скопипастила сюда свой код из задания 4))
+Он полностью подходит под рлстановку задачи.
+Это я, видимо, в задании 4 перестаралась
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class FinalTask1 {
+    public static void main(String[] args) {
+
+        String input;       //переменная для сохранения вводимого в консоли значения
+        int decNum;         //переменная для созранения результата (число в десятичном формате)
+
+        //Создать объект для ввода данных с консоли
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // Вывести сообщение о необходимости ввести число
+        System.out.println("Введите число в бинарном формате:");
+
+        //Считать данные из консоли с обработкой возможного исключения
+        try{
+            input = reader.readLine();
+            //Преобразовать введенное строковое значение в число в бинарном формате
+            if (checkForBinarFormat(input)) {
+                decNum = convert(input);
+                //Вывод на экран результата
+                System.out.println("Ваше число в десятичном формате = " + decNum);
+            }
+        }
+        catch (IOException e){
+            System.out.println("Кажется что-то пошло не так... \nПопробуйте начать с начала.");
+        }
+    }
+
+
+    //Метод для перевода числоя из двоичной чмстемы в десятичную
+    public static int convert(String str){
+        int n = 0;          //переменная для сохранения результата смещений
+        for (int i = 0; i < str.length(); i++) {
+            if ((n > 2000000000) || (n< -2000000000))
+                System.out.println("Введено слишком большое число! Результат конвертвции в десятичную систему будет не точным.");
+            n <<= 1;
+            if (str.charAt(i) == '1') n++;
+        }
+        return n;
+    }
+
+
+    //Метод для проверки на формат
+    public static boolean checkForBinarFormat(String str) {
+        boolean isBinarNum=true;
+        for (int i = 0; i < str.length(); i++)
+            if ((str.charAt(i) != '1') && (str.charAt(i) != '0')) {
+                isBinarNum = false;
+                System.out.println("Введено число не в бинарном формате! Попробуйте начать сначала.");
+                break;
+            }
+        return isBinarNum;
+    }
+
+
 }
