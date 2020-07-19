c733da3c027c6d00344ae98412071959c7a1d364
akuliniya <akuliniya@mail.ru>
2020-07-20 00:44:12 +0300

    removed the comma in System.out.print

--- src/testTask/Test9.java
+++ src/testTask/Test9.java
@@ -23,7 +23,7 @@ public class Test9 {
         }
 
         for(int i=0;i<array.length;i++){
-            System.out.print((array[i] * 2)+", ");
+            System.out.print((array[i] * 2)+" ");
         }
 
     }
