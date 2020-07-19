5dca63b72ba22156047d4f437db78013fcc70aa5
akuliniya <akuliniya@mail.ru>
2020-07-19 19:28:18 +0300

    Task 9 added

---
+++ src/testTask/Test9.java
@@ -0,0 +1,30 @@
+package testTask;
+
+/*
+Произведите ввод данных с клавиатуры в массив, а после этого произведите вывод массива на экран,
+где каждый элемент массива умножается на 2. Размер массива задается пользователем.
+ */
+
+import java.util.Scanner;
+
+public class Test9 {
+    public static void main(String[] args) {
+        int[] array;
+        int length;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        length = in.nextInt();
+        array = new int[length];
+
+        System.out.println("Заполните массив: ");
+        for(int i=0;i<length;i++){
+            array[i] = in.nextInt();
+        }
+
+        for(int i=0;i<array.length;i++){
+            System.out.print((array[i] * 2)+", ");
+        }
+
+    }
+}
