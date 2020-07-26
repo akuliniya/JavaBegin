85570a2ee28ff1e4aa31411c293cf5e18c0cfc59
akuliniya <akuliniya@mail.ru>
2020-07-26 23:03:30 +0300

    correction of mistakes

--- src/testTask/Test15.java
+++ src/testTask/Test15.java
@@ -9,29 +9,27 @@ import java.util.Scanner;
 
 public class Test15 {
     public static void main(String[] args) {
-        int[] array;
-        int length;
 
         Scanner in = new Scanner(System.in);
         System.out.print("Введите размер массива: ");
-        length = in.nextInt();
-        array = new int[length];
+        int length = in.nextInt();       //Перенесла объявление переменной
+        int[] array = new int[length];  //Перенесла объявление переменной
 
         System.out.println("Заполните массив: ");
-        for(int i=0;i<length;i++){
+        for(int i = 0; i < length; i++){  //Добавила пробелы
             array[i] = in.nextInt();
         }
 
-        for (int i=0; i < array.length; i++)
-            for (int j = 0; j<array.length-1; j++)
-                if (array[j]>array[j+1]){
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array.length - 1; j++) //Добавила пробелы
+                if (array[j] > array[j + 1]){
                     int tmp = array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=tmp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
                 }
 
-        for(int i=0;i<array.length;i++){
-            System.out.print(array[i]+" ");
+        for(int value : array){  //Заменила fo loop на enhanced for
+            System.out.print(value);
         }
 
     }
