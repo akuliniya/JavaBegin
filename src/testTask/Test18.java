e5f8d2b87657c0d82bedee2fa1976b4d504cf71f
akuliniya <akuliniya@mail.ru>
2020-07-27 00:36:30 +0300

    correction of mistakes

--- src/testTask/Test18.java
+++ src/testTask/Test18.java
@@ -9,49 +9,76 @@ import java.io.*;
 
 public class Test18 {
     public static void main(String[] args) {
-        String file = "C:\\Users\\user\\Desktop\\test16.txt";
-        String str;
+        String file = "test16.txt";
+        int linesBefore = 0;  //Добавила счетчик
+        int linesAfter = 0;  //Добавила счетчик
+
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String str = null;  //Проинициализировала
 
-            if((str = reader.readLine()) != null)
-                System.out.println("В файле "+file+" содержится текст:");
-
-            while((str = reader.readLine()) != null)
-                System.out.println(str);
-            System.out.println();
-
+                if (isFileEmpty(file)) {
+                    System.out.println("Файл пуст.");
+                    return;
+                }
+                else {
+                    System.out.println("В файле " + file + " содержится текст:");
+                    // Ваш комментарий "тут ошибка в цикле выведутся не все строчки". Исправила, добавила метод isFileEmpty для проверки
+                    while((str = reader.readLine()) != null){
+                        System.out.println(str);
+                        linesBefore++;
+                }
+            }
         }catch (FileNotFoundException e) {
             System.out.println("Файл не найден.\n"+e);
         }catch (IOException e) {
             System.out.println("Ошибка ввода-вывода.\n"+e);
         }
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Ведите новый текст для записи в файл.\n" +
-                "Прежний текст, если он имелся, будет перезаписан.\n" +
-                "Запись в файл завершится, когда вы введете: \"stop\"");
+        System.out.println("\nВедите новый текст для записи в файл.\n" +
+                "Прежний текст будет перезаписан.\n" +
+                "Запись будет прервана, когда количество введенных вами строк станет равно " + linesBefore);
 
-            try{
-                FileWriter writer = new FileWriter(file);
-                do{
-                    System.out.print(": ");
-                    str = reader.readLine();
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            FileWriter writer = new FileWriter(file);
+            String str = null;
 
-                    if (str.compareTo("stop") == 0)     break;
+            do{
+                System.out.print(": ");
+                str = reader.readLine();
+                linesAfter++;
 
+                if (linesAfter == linesBefore)  {
                     str += "\r\n";
                     writer.write(str);
+                    break;
+                    }
 
-                }while(str.compareTo("stop") != 0);
+                str += "\r\n";
+                writer.write(str);
+
+            }while(linesAfter < linesBefore);  // Исправила условие(выходить из цикла не по stop, а по кол-ву строк
 
                 writer.close();
 
-            }catch (IOException e){
-                System.out.println("Ошибка ввода или записи в файл.\n"+e.getMessage());
-            }
+        }catch (IOException e){
+            System.out.println("Ошибка ввода или записи в файл.\n"+e.getMessage());
+        }
 
     }
+
+
+    public static boolean isFileEmpty (String file) throws java.io.IOException{
+        boolean isEmpty = true;
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String str = null;
+        if((str = reader.readLine()) != null) {
+            isEmpty = false;
+        }
+        return isEmpty;
+    }
+
 }
