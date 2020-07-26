c7fdeef455f48a6fbf8682eba4a9c29125d8652f
akuliniya <akuliniya@mail.ru>
2020-07-26 21:39:28 +0300

    correction of mistakes

--- src/testTask/Test8.java
+++ src/testTask/Test8.java
@@ -6,7 +6,7 @@ package testTask;
 
 public class Test8 {
     public static void main(String[] args) {
-        for (int i=1;i<=50;i++)
+        for (int i = 1; i <= 50; i++)
             System.out.println(i);
     }
 }
